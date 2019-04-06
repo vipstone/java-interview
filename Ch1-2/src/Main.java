77381335ce6c943602563627c8399d8f7d1d4e48
wanglei <295628068@qq.com>
2019-04-06 18:20:13 +0800

    1-2 示例代码提交

---
+++ Ch1-2/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+public static void main(String[] args) {
+    // Byte 取值：-128 ~ 127
+    System.out.println(String.format("Byte 取值：%d ~ %d", Byte.MIN_VALUE, Byte.MAX_VALUE));
+    // Int 取值：-2147483648 ~ 2147483647
+    System.out.println(String.format("Int 取值：%d ~ %d", Integer.MIN_VALUE, Integer.MAX_VALUE));
+}
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# java-interview-course
+# Java 面试系列代码示例
+
+系统环境：
+
+- JDK 8
+- IDE：Intellij IDEA
