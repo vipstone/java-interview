e9a4bddc5b7ad505339dfc9f1e268b8e0c093925
wanglei <295628068@qq.com>
2019-04-07 21:42:38 +0800

    去掉无用代码

--- interview-demo/src/Main.java
+++ interview-demo/src/Main.java
@@ -2,7 +2,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("请去对应的文件目录，查找相应的代码示例~");
-        Integer num1;
-        System.out.println(""+num1);
     }
 }
