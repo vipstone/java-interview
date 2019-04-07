fd8a75a242dd6a0e877361ebc393d80a61f733d5
wanglei <295628068@qq.com>
2019-04-08 07:44:56 +0800

    增加示例代码

---
+++ interview-demo/src/com/interview/ch1/Ch1_02_03.java
@@ -0,0 +1,19 @@
+package com.interview.ch1;
+
+/*
+ * 第1章_02课_02 示例代码
+ * 演示 修改 Integer 高频区域的最大值
+ */
+public class Ch1_02_03 {
+    public static void main(String[] args) {
+        Integer num1 = -128;
+        Integer num2 = -128;
+        System.out.println("值为-128 => " + (num1 == num2));
+        Integer num3 = 666;
+        Integer num4 = 666;
+        System.out.println("值为666 => " + (num3 == num4));
+        Integer num5 = 667;
+        Integer num6 = 667;
+        System.out.println("值为667 => " + (num5 == num6));
+    }
+}
