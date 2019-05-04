8eb7dc2432898a2090b1789e50da4d40029fa905
stone <295628068@qq.com>
2019-05-04 20:22:35 +0800

    add String to Array

--- interview-demo/src/com/interview/ch1/Ch1_07.java
+++ interview-demo/src/com/interview/ch1/Ch1_07.java
@@ -1,5 +1,9 @@
 package com.interview.ch1;
 
+import com.sun.deploy.util.StringUtils;
+
+import java.util.Arrays;
+
 /**
  * @Description 第一章 第七节 代码示例
  * @Author wanglei
@@ -16,8 +20,33 @@ public class Ch1_07 {
 //        change(arr);
 //        System.out.println(arr[2]);
 
-        String[] arr = "hi,laowang".split(",");
-        System.out.println(arr[0]);
+        /*
+         * 字符串转数组
+         */
+//        String str = "laowang,stone,wanglei";
+//        String[] arr = str.split(","); // 字符串转数组
+//        System.out.println(arr[0]);
+
+        /*
+         * String To Array
+         */
+//        String[] arr = {"laowang", "stone", "wanglei"};
+//        // 方式一：遍历
+//        StringBuffer sb = new StringBuffer();
+//        for (int i = 0; i < arr.length; i++) {
+//            sb.append(arr[i]);
+//            if (i != arr.length - 1)
+//                sb.append(",");
+//        }
+//        System.out.println(sb.toString());
+//        // 方式二：Arrays.toString
+//        String str2 = Arrays.toString(arr);
+//        System.out.println(str2);
+//        // 方式三：StringUtils.join
+//        String str3 = StringUtils.join(Arrays.asList(arr), ","); // 使用英文逗号分隔
+//        System.out.println(str3);
+
+
 
     }
 
