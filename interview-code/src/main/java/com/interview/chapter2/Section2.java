e86f8cf1c773ee550a62d3cdaa61359debfc0d35
wanglei <295628068@qq.com>
2019-05-10 11:30:36 +0800

    clone使用示例添加

---
+++ interview-code/src/main/java/com/interview/chapter2/Section2.java
@@ -0,0 +1,29 @@
+package com.interview.chapter2;
+
+public class Section2 implements Cloneable {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        CloneTest ct = new CloneTest();
+        ct.num = 666;
+        System.out.println(ct.num);
+        CloneTest ct2 = (CloneTest) ct.clone();
+        System.out.println(ct2.num);
+    }
+}
+
+/**
+ * @Description 克隆实现
+ **/
+class CloneTest implements Cloneable {
+    int num;
+    public static void main(String[] args) throws CloneNotSupportedException {
+        CloneTest ct = new CloneTest();
+        ct.num = 666;
+        System.out.println(ct.num);
+        CloneTest ct2 = (CloneTest) ct.clone();
+        System.out.println(ct2.num);
+    }
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
