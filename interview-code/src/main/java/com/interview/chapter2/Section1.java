f5add3e27e54719a83cdaf151c7ba54eba85dd8e
stone <295628068@qq.com>
2019-05-12 08:47:39 +0800

    2-1 示例完善

--- interview-code/src/main/java/com/interview/chapter2/Section1.java
+++ interview-code/src/main/java/com/interview/chapter2/Section1.java
@@ -1,6 +1,6 @@
 package com.interview.chapter2;
 
-public class Section1  {
+public class Section1 {
     public static void main(String[] args) throws CloneNotSupportedException {
         /********** 克隆 **********/
         CloneTest ct = new CloneTest();
@@ -19,6 +19,7 @@ public class Section1  {
  **/
 class CloneTest implements Cloneable {
     int num;
+
     public static void main(String[] args) throws CloneNotSupportedException {
         CloneTest ct = new CloneTest();
         ct.num = 666;
@@ -26,6 +27,7 @@ class CloneTest implements Cloneable {
         CloneTest ct2 = (CloneTest) ct.clone();
         System.out.println(ct2.num);
     }
+
     @Override
     protected Object clone() throws CloneNotSupportedException {
         return super.clone();
@@ -40,4 +42,88 @@ class Animal {
 
 class Cat extends Animal {
 
+}
+
+/********** 继承执行顺序面试题 **********/
+class ExecTest {
+    public static void main(String[] args) {
+        Son son = new Son();
+    }
+}
+
+class Parent {
+    {
+        System.out.print("1");
+    }
+
+    static {
+        System.out.print("2");
+    }
+
+    public Parent() {
+        System.out.print("3");
+    }
+}
+
+class Son extends Parent {
+    {
+        System.out.print("4");
+    }
+
+    static {
+        System.out.print("5");
+    }
+
+    public Son() {
+        System.out.print("6");
+    }
+}
+
+/********* 继承面试题 **********/
+class A {
+    //    public int x = 0;
+//    public static int y = 0;
+    public void m(A a) {
+        System.out.println("AA");
+    }
+
+    public void m(D d) {
+        System.out.println("AD");
+    }
+
+//    public void m(B b) {
+//        System.out.println("AB");
+//    }
+}
+
+class B extends A {
+    //    public int x = 1;
+//    public static int y = 2;
+    @Override
+    public void m(A a) {
+        System.out.println("BA");
+    }
+
+    public void m(B b) {
+        System.out.println("BB");
+    }
+
+    public static void main(String[] args) {
+        A a = new B();
+        B b = new B();
+        C c = new C();
+        D d = new D();
+        a.m(a);
+        a.m(b);
+        a.m(c);
+        a.m(d);
+//        System.out.print(a.x);
+//        System.out.print(a.y);
+    }
+}
+
+class C extends B {
+}
+
+class D extends B {
 }
