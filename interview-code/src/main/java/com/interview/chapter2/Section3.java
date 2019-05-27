79a28978102f0e655c53c5cf97942acef06c9121
stone <295628068@qq.com>
2019-05-27 08:15:53 +0800

    2-3 完整示例代码提交

--- interview-code/src/main/java/com/interview/chapter2/Section3.java
+++ interview-code/src/main/java/com/interview/chapter2/Section3.java
@@ -1,3 +1,64 @@
 package com.interview.chapter2;
 
+abstract class AbstractAnimal {
+    public AbstractAnimal() {
+        System.out.println("Init AbstractAnimal.");
+    }
 
+    protected static String animalName = "AbstractAnimal";
+
+    abstract void eat();
+
+    public void run() {
+        System.out.println("AbstractAnimal Run.");
+    }
+}
+
+class AbstractAnimalInstance extends AbstractAnimal {
+    public static void main(String[] args) {
+        AbstractAnimal animal = new AbstractAnimalInstance();
+        animal.run();
+        System.out.println(animalName);
+        animal.eat();
+    }
+
+    @Override
+    void eat() {
+        System.out.println("AbstractAnimalInstance Eat.");
+    }
+}
+
+//@FunctionalInterface
+interface IAnimal {
+    static String animalName = "Animal Name";
+
+    static void printSex() {
+        System.out.println("Male Dog");
+    }
+
+    default void printAge() {
+        System.out.println("18");
+    }
+
+    void run(String name);
+}
+
+//class FunctionInterfaceTest {
+//    public static void main(String[] args) {
+//        IAnimal animal = name -> System.out.println(name);
+//        animal.run("WangWang");
+//    }
+//}
+
+class AnimalImpl implements IAnimal {
+    static String animalName = new String("Animal Name");
+
+    public static void main(String[] args) {
+        System.out.println(IAnimal.animalName == animalName);
+    }
+
+    @Override
+    public void run(String name) {
+        System.out.println("AnimalImpl：" + name);
+    }
+}
