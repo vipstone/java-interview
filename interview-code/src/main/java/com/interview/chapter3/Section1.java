f6b32020211e71b340144b10b807a0936ecbb48a
stone <295628068@qq.com>
2019-06-02 17:57:14 +0800

    3-1 示例代码提交

--- interview-code/src/main/java/com/interview/chapter3/Section1.java
+++ interview-code/src/main/java/com/interview/chapter3/Section1.java
@@ -4,47 +4,65 @@ import java.util.*;
 
 public class Section1 {
     public static void main(String[] args) {
-//        // -------------------------- Vector 使用代码 ----------------------------
-//        Vector vector = new Vector();
-//        vector.add("dog");
-//        vector.add("cat");
-//        vector.remove("cat");
-//        System.out.println(vector);
-//
-//        // -------------------------- ArrayList 使用代码 ----------------------------
-//        ArrayList arrayList = new ArrayList();
-//        arrayList.add("dog");
-//        arrayList.add("cat");
-//        arrayList.remove("cat");
-//        System.out.println(arrayList);
-//
-//        // -------------------------- LinkedList 使用代码 ----------------------------
-//        LinkedList linkedList = new LinkedList();
-//        // 添加元素
-//        linkedList.offer("bird");
-//        linkedList.push("cat");
-//        linkedList.push("dog");
-//        // 获取第一个元素
-//        System.out.println(linkedList.peek());
-//        // 获取第一个元素，并删除此元素
-//        System.out.println(linkedList.poll());
-//        System.out.println(linkedList);
+        // -------------------------- Vector 使用代码 ----------------------------
+        Vector vector = new Vector();
+        vector.add("dog");
+        vector.add("cat");
+        vector.remove("cat");
+        System.out.println(vector);
+
+        // -------------------------- ArrayList 使用代码 ----------------------------
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("dog");
+        arrayList.add("cat");
+        arrayList.remove("cat");
+        System.out.println(arrayList);
+
+        // -------------------------- LinkedList 使用代码 ----------------------------
+        LinkedList linkedList = new LinkedList();
+        // 添加元素
+        linkedList.offer("bird");
+        linkedList.push("cat");
+        linkedList.push("dog");
+        // 获取第一个元素
+        System.out.println(linkedList.peek());
+        // 获取第一个元素，并删除此元素
+        System.out.println(linkedList.poll());
+        System.out.println(linkedList);
 
         // -------------------------- HashSet 使用代码 ----------------------------
-        System.out.println("-------------- HashSet -----------------");
         HashSet hashSet = new HashSet();
         hashSet.add("dog");
-        hashSet.add("color");
+        hashSet.add("camel");
         hashSet.add("cat");
         System.out.println(hashSet);
 
         // -------------------------- TreeSet 使用代码 ----------------------------
-        System.out.println("-------------- TreeSet -----------------");
         TreeSet treeSet = new TreeSet();
         treeSet.add("dog");
-        treeSet.add("color");
+        treeSet.add("camel");
         treeSet.add("cat");
+        treeSet.add("ant");
         System.out.println(treeSet);
+
+        // -------------------------- LinkedHashSet 使用代码 ----------------------------
+        LinkedHashSet linkedHashSet = new LinkedHashSet();
+        linkedHashSet.add("dog");
+        linkedHashSet.add("camel");
+        linkedHashSet.add("cat");
+        linkedHashSet.add("ant");
+        System.out.println(linkedHashSet);
+
+        // -------------------------- 集合与数组的互相转换 ----------------------------
+        List<String> list = new ArrayList();
+        list.add("cat");
+        list.add("dog");
+        // 集合转数组
+        String[] arr = list.toArray(new String[list.size()]);
+        // 数组转集合
+        List<String> list2 = Arrays.asList(arr);
+        System.out.println(list2);
+
 
 
 
