a1d8a6b0b52735536cc918f8c8908cd0b7da9023
wanglei <295628068@qq.com>
2019-06-12 16:49:48 +0800

    添加延迟队列实现示例

---
+++ interview-code/src/main/java/com/interview/chapter3/CustomDelayQueue.java
@@ -0,0 +1,111 @@
+package com.interview.chapter3;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/*
+ * 如何使用Java原生方式，手撸一个延迟队列（MQ）？多生产者版本
+ **/
+public class CustomDelayQueue {
+    // 消息编号
+    static AtomicInteger MESSAGENO = new AtomicInteger(1);
+
+    public static void main(String[] args) throws InterruptedException {
+        DelayQueue<DelayedElement> delayQueue = new DelayQueue<>();
+        // 生产者1
+        producer(delayQueue, "生产者1");
+
+        // 生产者2
+        producer(delayQueue, "生产者2");
+
+        // 消费者
+        consumer(delayQueue);
+
+        /* 执行结果
+            生产者1，消息编号：1 发送时间：2019-6-12 20:38:37 延迟：2 秒 |执行时间：2019-6-12 20:38:39
+            生产者2，消息编号：2 发送时间：2019-6-12 20:38:37 延迟：2 秒 |执行时间：2019-6-12 20:38:39
+            生产者1，消息编号：3 发送时间：2019-6-12 20:38:41 延迟：4 秒 |执行时间：2019-6-12 20:38:45
+            生产者1，消息编号：5 发送时间：2019-6-12 20:38:43 延迟：2 秒 |执行时间：2019-6-12 20:38:45
+            ....
+         */
+    }
+
+    //生产者
+    private static void producer(DelayQueue<DelayedElement> delayQueue, String name) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    // 产生 1~5 秒的随机数
+                    long time = 1000L * (new Random().nextInt(5) + 1);
+                    try {
+                        Thread.sleep(time);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    // 组合消息体
+                    String message = String.format("%s，消息编号：%s 发送时间：%s 延迟：%s 秒",
+                            name, MESSAGENO.getAndIncrement(), new Date().toLocaleString(), time / 1000);
+                    // 生产消息
+                    delayQueue.put(new DelayedElement(message, time));
+                }
+            }
+        }).start();
+    }
+
+    //消费者
+    private static void consumer(DelayQueue<DelayedElement> delayQueue) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    DelayedElement element = null;
+                    try {
+                        // 消费消息
+                        element = delayQueue.take();
+                        System.out.println(element);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    // 延迟队列对象
+    static class DelayedElement implements Delayed {
+        // 过期时间（单位：毫秒）
+        long time = System.currentTimeMillis();
+        // 消息体
+        String message;
+        // 参数：delayTime 延迟时间（单位毫秒）
+        public DelayedElement(String message, long delayTime) {
+            this.time += delayTime;
+            this.message = message;
+        }
+        @Override
+        // 获取过期时间
+        public long getDelay(TimeUnit unit) {
+            return unit.convert(time - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+        }
+        @Override
+        // 队列元素排序
+        public int compareTo(Delayed o) {
+            if (this.getDelay(TimeUnit.MILLISECONDS) > o.getDelay(TimeUnit.MILLISECONDS))
+                return 1;
+            else if (this.getDelay(TimeUnit.MILLISECONDS) < o.getDelay(TimeUnit.MILLISECONDS))
+                return -1;
+            else
+                return 0;
+        }
+        @Override
+        public String toString() {
+            // 打印消息
+            return message + " |执行时间：" + new Date().toLocaleString();
+        }
+    }
+}
---
+++ interview-code/src/main/java/com/interview/chapter3/Section4.java
@@ -0,0 +1,90 @@
+package com.interview.chapter3;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+public class Section4 {
+    static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public static void main(String[] args) throws InterruptedException {
+
+//        // --------------------- LinkedList 使用 ---------------------
+//        Queue<String> linkedList = new LinkedList<>();
+//        linkedList.add("Dog");
+//        linkedList.add("Camel");
+//        linkedList.add("Cat");
+//        for (String item : linkedList) {
+//            System.out.println(item);
+//        }
+
+//        // --------------------- PriorityQueue 使用 ---------------------
+//        Queue<Integer> priorityQueue = new PriorityQueue(new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer o1, Integer o2) {
+//                // 非自然排序，数字倒序
+//                return o2 - o1;
+//            }
+//        });
+//        priorityQueue.add(3);
+//        priorityQueue.add(1);
+//        priorityQueue.add(2);
+//        while (!priorityQueue.isEmpty()) {
+//            Integer i = priorityQueue.poll();
+//            System.out.println(i);
+//        }
+
+        // --------------------- DelayQueue 延迟队列 ---------------------
+        DelayQueue delayQueue = new DelayQueue();
+        delayQueue.put(new DelayElement(1000));
+        delayQueue.put(new DelayElement(3000));
+        delayQueue.put(new DelayElement(5000));
+        System.out.println("当前时间：" + simpleDateFormat.format(new Date()));
+        while (!delayQueue.isEmpty()){
+            System.out.println(delayQueue.take());
+        }
+        System.out.println("结束时间：" + simpleDateFormat.format(new Date()));
+        /* 执行结果：
+            当前时间：2019-06-12 20:34:55
+            2019-06-12 20:34:56
+            2019-06-12 20:34:58
+            2019-06-12 20:35:00
+            结束时间：2019-06-12 20:35:00
+         */
+
+    }
+
+    static class DelayElement implements Delayed {
+        // 延迟截止时间（单面：毫秒）
+        long delayTime = System.currentTimeMillis();
+        public DelayElement(long delayTime) {
+            this.delayTime = (this.delayTime + delayTime);
+        }
+        @Override
+        // 获取剩余时间
+        public long getDelay(TimeUnit unit) {
+            return unit.convert(delayTime - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+        }
+        @Override
+        // 队列里元素的排序依据
+        public int compareTo(Delayed o) {
+            if (this.getDelay(TimeUnit.MILLISECONDS) > o.getDelay(TimeUnit.MILLISECONDS)) {
+                return 1;
+            } else if (this.getDelay(TimeUnit.MILLISECONDS) < o.getDelay(TimeUnit.MILLISECONDS)) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+        @Override
+        public String toString() {
+            return simpleDateFormat.format(new Date(delayTime));
+        }
+    }
+
+}
+
+
