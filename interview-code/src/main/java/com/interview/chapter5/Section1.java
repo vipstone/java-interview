d1db7f1a1915c58c79067eb4a1afe44dd533618e
wanglei <295628068@qq.com>
2019-06-22 17:55:55 +0800

    增加线程开发示例

---
+++ interview-code/src/main/java/com/interview/chapter5/Section1.java
@@ -0,0 +1,111 @@
+package com.interview.chapter5;
+
+import javafx.scene.input.DataFormat;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.Callable;
+
+class ThreadTest {
+    public static void main(String[] args) throws Exception {
+//        // 创建方式一：继承 Thread 类
+//        MyThread thread = new MyThread();
+//        thread.start();
+//        // 创建方式二：实现 Runnable 接口
+//        MyRunnable runnable = new MyRunnable();
+//        runnable.run();
+//        // 创建方式三：实现 Callable 接口
+//        MyCallable callable  = new MyCallable();
+//        Object result = callable.call();
+//        System.out.println(result);
+//        // JDK 8 lambda Thread
+//        new Thread(() -> System.out.println("Lambda Of Thread.")).start();
+//        // 设置线程优先级
+//        Thread priorityThread = new Thread(() -> System.out.println("Java"));
+//        priorityThread.setPriority(10);
+//        priorityThread.start();
+        //线程休眠
+        System.out.println(LocalDateTime.now());
+        Object lock = new Object();
+        Thread lockThread = new Thread(() -> {
+            synchronized (lock) {
+                try {
+                    // 1 秒钟之后自动唤醒
+                    lock.wait(1000);
+                    System.out.println(LocalDateTime.now());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        lockThread.start();
+
+//        // 休眠 1 秒
+//        Thread.sleep(1000);
+
+
+
+//        // 死锁演示
+//        Object obj1 = new Object();
+//        Object obj2 = new Object();
+//        // 线程1拥有对象1，想要等待获取对象2
+//        new Thread() {
+//            @Override
+//            public void run() {
+//                synchronized (obj1) {
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                    synchronized (obj2) {
+//                        System.out.println(Thread.currentThread().getName());
+//                    }
+//                }
+//            }
+//        }.start();
+//        // 线程2拥有对象2，想要等待获取对象1
+//        new Thread() {
+//            @Override
+//            public void run() {
+//                synchronized (obj2) {
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                    synchronized (obj1) {
+//                        System.out.println(Thread.currentThread().getName());
+//                    }
+//                }
+//            }
+//        }.start();
+
+    }
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Thread");
+    }
+}
+
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Runnable");
+    }
+}
+
+class MyCallable implements Callable {
+    @Override
+    public Object call() throws Exception {
+        System.out.println("Callable");
+        return "Success";
+    }
+}
+
+
