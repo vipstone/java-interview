186a8992fa9c7ea86ec1c6e5bb04f822f43a89a4
wanglei <295628068@qq.com>
2019-08-07 16:45:23 +0800

    增加添加跳出多重循环

--- interview-code/src/main/java/com/interview/Lesson1_4.java
+++ interview-code/src/main/java/com/interview/Lesson1_4.java
@@ -4,6 +4,29 @@ public class Lesson1_4 {
     static int count = 0;
 
     public static void main(String[] args) {
+        // 方式一，跳出多重循环
+        myfor:
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                System.out.println("J:" + j);
+                if (j == 10) {
+                    // 跳出多重循环
+                    break myfor;
+                }
+            }
+        }
+        // 方式二，跳出多重循环
+        boolean flag = true;
+        for (int i = 0; i < 100 && flag; i++) {
+            for (int j = 0; j < 100; j++) {
+                System.out.println("J:" + j);
+                if (j == 10) {
+                    // 跳出多重循环
+                    flag = false;
+                    break;
+                }
+            }
+        }
         // 循环永远无法停下来
 //        for (float i = 0; i != 1; i += 0.1) {
 //            System.out.println(i);
