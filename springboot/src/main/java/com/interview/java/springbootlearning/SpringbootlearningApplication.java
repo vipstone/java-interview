0b1231807bc8e70e39e65d2848440e0372937155
wanglei <295628068@qq.com>
2019-08-27 16:07:37 +0800

    添加 Spring Boot 示例

---
+++ springboot/src/main/java/com/interview/java/springbootlearning/SpringbootlearningApplication.java
@@ -0,0 +1,13 @@
+package com.interview.java.springbootlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootlearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootlearningApplication.class, args);
+    }
+
+}
